7475372dc5786e46bf1c1c0f9dc3b1d517d4bf98
Paramjeet Pradhan <135497322+paramjeetn@users.noreply.github.com>
2024-02-11 17:52:30 +0530

    Update Authentication.jsx

--- frontend/src/components/Authentication.jsx
+++ frontend/src/components/Authentication.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from './Auth';
 import Weather from './Weather';
+import Glogin from "./Glogin";
+
 // import '../public/css/Authentication.css'
 
 function Authentication(){
@@ -12,6 +14,7 @@ function Authentication(){
                   <BrowserRouter>
                       <Routes>
                             <Route path="/" element={<Auth />} />
+                            <Route path="/glogin" element={<Glogin />} />
                             <Route path="/weather" element={<Weather />} />
                             {/* <Route exact path="/" component={Auth} />
                             <Route path="/weather" component={Weather} /> */}
